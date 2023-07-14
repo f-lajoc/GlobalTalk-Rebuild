f4176f92ee4b385de822563cab2ec3b913ef77aa
Lajoc_devs <104025961+f-lajoc@users.noreply.github.com>
2023-07-14 20:57:18 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./**/*.{html,js}"],
+	theme: {
+		extend: {
+			fontFamily: { redHat: ['"Red Hat Display" ', "cursive"] }
+		},
+	},
+	plugins: [],
+};
+
